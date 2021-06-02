c92801c9fae491d929d35df769a0d5c433e91c43
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-06-01 21:12:24 -0500

    ADD: add function to connect with db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
+const db = require('./config/db');
 const server = require('./server/server');
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Server listen on port: ${port}`));
+server.listen(port, () => {
+	console.log(`Server listen on port: ${port}`);
+	db.sync()
+		.then(() => console.log('Database Connected'))
+		.catch(error => console.log('Este es es el error: ', error));
+});
